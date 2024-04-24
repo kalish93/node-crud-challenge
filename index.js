1656a1c482d429cde19386593b8f0ecf50e2abb1
kalish93 <kaleabtekalign@gmail.com>
2024-04-24 13:23:35 +0300

    feat: person crud

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
 
 let persons = [{
     id: '1',
@@ -9,7 +11,57 @@ let persons = [{
 }] //This is your in memory database
 
 app.set('db', persons)
-//TODO: Implement crud of person
+app.use(express.json())
+app.use(cors())
+
+//Get all persons
+app.get('/person', (req, res)=>{
+    res.status(200).json(persons);
+})
+
+//Create a person
+app.post('/person', (req, res)=>{
+    const newPerson = req.body;
+    newPerson.id = uuidv4();
+    persons.push(newPerson);
+    res.status(201).json(newPerson); 
+})
+
+//Get a single person
+app.get('/person/:personId', (req, res) =>{
+    const id = req.params.personId;
+    const person = persons.find((p) => p.id === id);
+    if(person){
+    res.status(200).json(person);
+    }else{
+        res.status(404).json({'message': 'Person not found.'})
+    }
+})
+
+//Update a person
+app.put('/person/:personId', (req, res) =>{
+    const id = req.params.personId;
+    const updatedPerson = req.body;
+    const index = persons.findIndex((p) => p.id === id);
+    if(index !== -1){
+        persons[index] = {...persons[index], ...updatedPerson}
+        res.status(200).json(persons[index]);
+    }else{
+        res.status(404).json({'message': 'Person not found.'})
+    }
+})
+
+//Update a person
+app.delete('/person/:personId', (req, res) =>{
+    const id = req.params.personId;
+    persons = persons.filter((p) => p.id !== id);
+    res.status(204).send("Person deleted successfully.'")
+    
+})
+
+app.use((req, res, next) => {
+    res.status(404).send('404 Not Found');
+});
 
 if (require.main === module) {
     app.listen(3000)
